d85233d4624ecf5c87e54c74e4bed7e1986e1b4b
Wout.Verbiest <wout.verbiest@student.howest.be>
2020-05-31 00:07:28 +0200

    add icons and expand ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 void main() => runApp(MaterialApp(
       title: "Weather App",
@@ -54,7 +57,36 @@ class _HomeState extends State<Home> {
                     ),
                   ),
                 ],
-              ))
+              )),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: ListView(
+                children: <Widget>[
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.thermometerHalf),
+                    title: Text("Temperature"),
+                    trailing: Text("24\u00B0"),
+                  ),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.cloud),
+                    title: Text("Weather"),
+                    trailing: Text("sunny"),
+                  ),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.sun),
+                    title: Text("Humidity"),
+                    trailing: Text("15"),
+                  ),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.wind),
+                    title: Text("Wind Speed"),
+                    trailing: Text("12"),
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
